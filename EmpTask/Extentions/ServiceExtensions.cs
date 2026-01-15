7a9d6c171dd91ff90afa63dfddea412b05de7d21
ahmed helal <ahmed.abdelrasheed@nationaltechnology.net>
2026-01-15 13:56:49 +0200

    Emptask v1

---
+++ EmpTask/Extentions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using EmpTask.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpTask.Extentions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureProviders(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
---
+++ EmpTask/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace EmpTask.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public string ImageUrl { get; set; }
+        public string ManagerName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
